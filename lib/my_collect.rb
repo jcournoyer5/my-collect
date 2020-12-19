58e834cd1a75ae51fd17b329efbda19930e91024
Jacqueline Cournoyer <jcournoyer5@gmail.com>
2020-12-19 03:46:35 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,4 @@
-brothers = ["Tom", "Tim", "Jim"]
-
-count = 0
-while count <= brothers.length-1
- puts "Stop hitting yourself #{brothers[count]}!"
- count += 1
+students = [Jane, Mark, Linda]
+my_collect(students) do |student|
+ student.split(" ").first
 end
