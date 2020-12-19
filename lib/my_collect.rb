458768350c83f138459aab16e47b798ce5b79605
Jacqueline Cournoyer <jcournoyer5@gmail.com>
2020-12-19 02:27:23 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,1 +1,13 @@
+ def my_collect
+ my_collect(Array) do |x|
+         thing += 1
+       end
 
+(my_collect(languages) do |language|
+         language.upcase
+       end).to eq(["RUBY", "JAVASCRIPT", "PYTHON", "OBJECTIVE-C"])
+     
+     my_collect(students) do |student|
+          student.split(" ").first
+      end
+ end
