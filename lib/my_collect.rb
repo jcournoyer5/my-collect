7f179b0926d738f147c9ca99c92520568ad8e664
Jacqueline Cournoyer <jcournoyer5@gmail.com>
2020-12-19 05:12:34 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,14 @@
-def my_collect(languages)
- languages do |languages|
-yield(languages)
- return languages.upcase
-  end
+def my_collect(array)
+	result = []
 end
+
+
+
+#def my_collect(languages)
+# languages do |languages|
+#yield(languages)
+ #return languages.upcase
+  #end
+#end
    
 
