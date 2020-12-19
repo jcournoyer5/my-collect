bd2beedd1a6dde3780bc1be91f311d90f4128a69
Jacqueline Cournoyer <jcournoyer5@gmail.com>
2020-12-19 02:45:57 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,6 @@
-baked_goods = "[babka, muffins, tarts]"
 
-def menu(baked_goods)
-    my_collect(baked_goods) do |name|
-  name.split(" ").first
-end 
-end
+   def my_collect
+   my_collect(empty) do |x|
+         counter += 1
+       end
+   end
